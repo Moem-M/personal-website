1c9c914d1cc250d3f18acc26cb2c025f603db9b6
Moem <156375920+Moem-M@users.noreply.github.com>
2026-04-25 21:30:24 +0300

    contact page skeleton

---
+++ pages/contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>Contact</title>
+    <link rel="stylesheet" type="text/css" href="../global/styles.css" />
+    <script src="../scripts/email.js"></script>
+</head>
+
+<body>
+    <?php include("../includes/header.php"); ?>
+    <?php include("../includes/links.php"); ?>
+    <div class="main-content">
+        <h1 id="contact-header" class="main-header">Contact Me</h1>
+        <div id="contact-content">
+            <p id="email">Email Me :<span id="emailAddress"><script>showEmail("redacted", "redacted")</script></span></p>
+        </div>
+    </div>
+    <?php include("../includes/footer.php"); ?>
+</body>
+
+</html>
