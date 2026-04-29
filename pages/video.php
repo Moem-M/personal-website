1dd9933d2077c503387fc0b99961ae93ecf3aabf
Moem <156375920+Moem-M@users.noreply.github.com>
2026-04-29 19:57:11 +0300

    added resume and video pages

---
+++ pages/video.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>Video</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../global/styles.css" />
+    <link rel="icon" href="../images/logo500.png" />
+</head>
+
+<body>
+    <?php include("../includes/header.php"); ?>
+    <?php include("../includes/links.php"); ?>
+    <div class="main-content" id="video-page">
+        <object id="video-embed" data=""></object>
+    </div>
+    <?php include("../includes/footer.php"); ?>
+</body>
+
+</html>
