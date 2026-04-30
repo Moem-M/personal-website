36a800c92a2a264ce88b966e26ee37740bcd08dd
Moem <156375920+Moem-M@users.noreply.github.com>
2026-04-30 22:00:45 +0300

    xhtml validation

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,7 @@
 <div id="footer">
     <footer>
-        <img src="" alt="w3c html">
-        <img src="" alt="w3c css">
+
+        <a href="https://validator.w3.org/check?uri=referer"> <img src="https://www.w3.org/Icons/valid-xhtml10" alt="w3c html" /></a>
+        <a href="https://jigsaw.w3.org/css-validator/check/referer"><img src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="w3c css" /></a>
     </footer>
 </div>
