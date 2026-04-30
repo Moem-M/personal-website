7cd4b3bc65193d50075933972cd04fdeb9bc7eb4
Moem <156375920+Moem-M@users.noreply.github.com>
2026-04-30 22:26:56 +0300

    css validation

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,10 @@
 <div id="footer">
-        <a href="https://validator.w3.org/check?uri=referer"> <img src="https://www.w3.org/Icons/valid-xhtml10" alt="w3c html" /></a>
-        <a href="https://jigsaw.w3.org/css-validator/check/referer"><img src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="w3c css" /></a>
+    <a href="https://validator.w3.org/check?uri=referer">
+        <img src="https://www.w3.org/Icons/valid-xhtml10" alt="w3c html" />
+    </a>
+
+    <a href="https://jigsaw.w3.org/css-validator/validator?uri=https://moem.cloud/global/styles.css">
+        <img src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="w3c css" />
+    </a>
+
 </div>
