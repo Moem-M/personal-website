41ba35321cb051794687227778de8a03d87e2b52
Moem <156375920+Moem-M@users.noreply.github.com>
2026-04-30 21:52:23 +0300

    schedule printing

--- pages/schedule.php
+++ pages/schedule.php
@@ -5,6 +5,7 @@
     <title>F1 2026 Schedule</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../global/styles.css" />
+    <link rel="stylesheet" type="text/css" media="print" href="../styles/print.css" />
     <link rel="icon" href="../images/logo500.png" />
 </head>
 
@@ -14,182 +15,183 @@
     <div class="main-content">
         <div id="schedule-div">
             <table id="f1-schedule">
-                <tr id="schedule-title-row">
-                    <th colspan="5">F1 2026 Schedule</th>
-                </tr>
-                <tr id="schedule-header-row">
-                    <th class="schedule-header">Grand Prix</th>
-                    <th class="schedule-header">Location</th>
-                    <th class="schedule-header">Month</th>
-                    <th class="schedule-header">Days</th>
-                    <th class="schedule-header">Race Start (Track Time)</th>
-                </tr>
+                <tbody id="schedule-tbody">
+                    <tr id="schedule-title-row">
+                        <th colspan="5">F1 2026 Schedule</th>
+                    </tr>
+                    <tr id="schedule-header-row">
+                        <th class="schedule-header">Grand Prix</th>
+                        <th class="schedule-header">Location</th>
+                        <th class="schedule-header">Month</th>
+                        <th class="schedule-header">Days</th>
+                        <th class="schedule-header">Race Start (Track Time)</th>
+                    </tr>
 
-                <!-- March (3 races) -->
-                <tr>
-                    <td>FORMULA 1 QATAR AIRWAYS AUSTRALIAN GRAND PRIX 2026</td>
-                    <td>Australia</td>
-                    <td rowspan="3">Mar</td>
-                    <td>06 - 08</td>
-                    <td rowspan="2">15:00</td>
-                </tr>
+                    <!-- March (3 races) -->
+                    <tr>
+                        <td>FORMULA 1 QATAR AIRWAYS AUSTRALIAN GRAND PRIX 2026</td>
+                        <td>Australia</td>
+                        <td rowspan="3">Mar</td>
+                        <td>06 - 08</td>
+                        <td rowspan="2">15:00</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 CHINESE GRAND PRIX 2026</td>
-                    <td>China</td>
-                    <td>13 - 15</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 CHINESE GRAND PRIX 2026</td>
+                        <td>China</td>
+                        <td>13 - 15</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 ARAMCO JAPANESE GRAND PRIX 2026</td>
-                    <td>Japan</td>
-                    <td>27 - 29</td>
-                    <td>14:00</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 ARAMCO JAPANESE GRAND PRIX 2026</td>
+                        <td>Japan</td>
+                        <td>27 - 29</td>
+                        <td>14:00</td>
+                    </tr>
 
-                <!-- May (2 races) -->
+                    <!-- May (2 races) -->
 
-                <tr>
-                    <td>FORMULA 1 CRYPTO.COM MIAMI GRAND PRIX 2026</td>
-                    <td>Miami - USA</td>
-                    <td rowspan="2">May</td>
-                    <td>01 - 03</td>
-                    <td rowspan="2">16:00</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 CRYPTO.COM MIAMI GRAND PRIX 2026</td>
+                        <td>Miami - USA</td>
+                        <td rowspan="2">May</td>
+                        <td>01 - 03</td>
+                        <td rowspan="2">16:00</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 LENOVO GRAND PRIX DU CANADA 2026</td>
-                    <td>Canada</td>
-                    <td>22 - 24</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 LENOVO GRAND PRIX DU CANADA 2026</td>
+                        <td>Canada</td>
+                        <td>22 - 24</td>
+                    </tr>
 
-                <!-- June (3 races) -->
-                <tr>
-                    <td>FORMULA 1 LOUIS VUITTON GRAND PRIX DE MONACO 2026</td>
-                    <td>Monaco</td>
-                    <td rowspan="3">Jun</td>
-                    <td>05 - 07</td>
-                    <td rowspan="10">15:00</td>
-                </tr>
+                    <!-- June (3 races) -->
+                    <tr>
+                        <td>FORMULA 1 LOUIS VUITTON GRAND PRIX DE MONACO 2026</td>
+                        <td>Monaco</td>
+                        <td rowspan="3">Jun</td>
+                        <td>05 - 07</td>
+                        <td rowspan="10">15:00</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 MSC CRUISES GRAN PREMIO DE BARCELONA-CATALUNYA 2026</td>
-                    <td>Barcelona - Spain</td>
-                    <td>12 - 14</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 MSC CRUISES GRAN PREMIO DE BARCELONA-CATALUNYA 2026</td>
+                        <td>Barcelona - Spain</td>
+                        <td>12 - 14</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 LENOVO AUSTRIAN GRAND PRIX 2026</td>
-                    <td>Austria</td>
-                    <td>26 - 28</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 LENOVO AUSTRIAN GRAND PRIX 2026</td>
+                        <td>Austria</td>
+                        <td>26 - 28</td>
+                    </tr>
 
-                <!-- July (3 races) -->
+                    <!-- July (3 races) -->
 
-                <tr>
-                    <td>FORMULA 1 PIRELLI BRITISH GRAND PRIX 2026</td>
-                    <td>Great Britain</td>
-                    <td rowspan="3">Jul</td>
-                    <td>03 - 05</td>
-                </tr>
-                <tr>
-                    <td>FORMULA 1 BELGIAN GRAND PRIX 2026</td>
-                    <td>Belgium</td>
-                    <td>17 - 19</td>
-                </tr>
-                <tr>
-                    <td>FORMULA 1 AWS HUNGARIAN GRAND PRIX 2026</td>
-                    <td>Hungary</td>
-                    <td>24 - 26</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 PIRELLI BRITISH GRAND PRIX 2026</td>
+                        <td>Great Britain</td>
+                        <td rowspan="3">Jul</td>
+                        <td>03 - 05</td>
+                    </tr>
+                    <tr>
+                        <td>FORMULA 1 BELGIAN GRAND PRIX 2026</td>
+                        <td>Belgium</td>
+                        <td>17 - 19</td>
+                    </tr>
+                    <tr>
+                        <td>FORMULA 1 AWS HUNGARIAN GRAND PRIX 2026</td>
+                        <td>Hungary</td>
+                        <td>24 - 26</td>
+                    </tr>
 
-                <!-- August (1 race) -->
+                    <!-- August (1 race) -->
 
-                <tr>
-                    <td>FORMULA 1 DUTCH GRAND PRIX 2026</td>
-                    <td>Netherlands</td>
-                    <td>Aug</td>
-                    <td>21 - 23</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 DUTCH GRAND PRIX 2026</td>
+                        <td>Netherlands</td>
+                        <td>Aug</td>
+                        <td>21 - 23</td>
+                    </tr>
 
-                <!-- September (3 races) -->
+                    <!-- September (3 races) -->
 
-                <tr>
-                    <td>FORMULA 1 PIRELLI GRAN PREMIO D’ITALIA 2026</td>
-                    <td>Italy</td>
-                    <td rowspan="3">Sep</td>
-                    <td>04 - 06</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 PIRELLI GRAN PREMIO D’ITALIA 2026</td>
+                        <td>Italy</td>
+                        <td rowspan="3">Sep</td>
+                        <td>04 - 06</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 TAG HEUER GRAN PREMIO DE ESPAÑA 2026</td>
-                    <td>Madrid - Spain</td>
-                    <td>11 - 13</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 TAG HEUER GRAN PREMIO DE ESPAÑA 2026</td>
+                        <td>Madrid - Spain</td>
+                        <td>11 - 13</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 QATAR AIRWAYS AZERBAIJAN GRAND PRIX 2026</td>
-                    <td>Azerbaijan</td>
-                    <td>24 - 26</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 QATAR AIRWAYS AZERBAIJAN GRAND PRIX 2026</td>
+                        <td>Azerbaijan</td>
+                        <td>24 - 26</td>
+                    </tr>
 
-                <!-- October (2 races) -->
+                    <!-- October (2 races) -->
 
-                <tr>
-                    <td>FORMULA 1 SINGAPORE AIRLINES SINGAPORE GRAND PRIX 2026</td>
-                    <td>Singapore</td>
-                    <td rowspan="2">Oct</td>
-                    <td>09 - 11</td>
-                    <td>20:00</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 SINGAPORE AIRLINES SINGAPORE GRAND PRIX 2026</td>
+                        <td>Singapore</td>
+                        <td rowspan="2">Oct</td>
+                        <td>09 - 11</td>
+                        <td>20:00</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 MSC CRUISES UNITED STATES GRAND PRIX 2026</td>
-                    <td>Texas - USA</td>
-                    <td>23 - 25</td>
-                    <td>15:00</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 MSC CRUISES UNITED STATES GRAND PRIX 2026</td>
+                        <td>Texas - USA</td>
+                        <td>23 - 25</td>
+                        <td>15:00</td>
+                    </tr>
 
-                <!-- October/November (1 race) -->
+                    <!-- October/November (1 race) -->
 
-                <tr>
-                    <td>FORMULA 1 GRAN PREMIO DE LA CIUDAD DE MÉXICO 2026</td>
-                    <td>Mexico</td>
-                    <td>Oct/Nov</td>
-                    <td>30 Oct - 01 Nov</td>
-                    <td rowspan="2">14:00</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 GRAN PREMIO DE LA CIUDAD DE MÉXICO 2026</td>
+                        <td>Mexico</td>
+                        <td>Oct/Nov</td>
+                        <td>30 Oct - 01 Nov</td>
+                        <td rowspan="2">14:00</td>
+                    </tr>
 
-                <!-- November (4 races) -->
+                    <!-- November (4 races) -->
 
-                <tr>
-                    <td>FORMULA 1 MSC CRUISES GRANDE PRÊMIO DE SÃO PAULO 2026</td>
-                    <td>Brazil</td>
-                    <td rowspan="4">Nov</td>
-                    <td>06 - 08</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 MSC CRUISES GRANDE PRÊMIO DE SÃO PAULO 2026</td>
+                        <td>Brazil</td>
+                        <td rowspan="4">Nov</td>
+                        <td>06 - 08</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 LAS VEGAS GRAND PRIX 2026</td>
-                    <td>Las Vegas - USA</td>
-                    <td>19 - 21</td>
-                    <td>20:00</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 LAS VEGAS GRAND PRIX 2026</td>
+                        <td>Las Vegas - USA</td>
+                        <td>19 - 21</td>
+                        <td>20:00</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 QATAR AIRWAYS QATAR GRAND PRIX 2026</td>
-                    <td>Qatar</td>
-                    <td>27 - 29</td>
-                    <td>19:00</td>
-                </tr>
+                    <tr>
+                        <td>FORMULA 1 QATAR AIRWAYS QATAR GRAND PRIX 2026</td>
+                        <td>Qatar</td>
+                        <td>27 - 29</td>
+                        <td>19:00</td>
+                    </tr>
 
-                <tr>
-                    <td>FORMULA 1 ETIHAD AIRWAYS ABU DHABI GRAND PRIX 2026</td>
-                    <td>Abu Dhabi</td>
-                    <td>04 - 06</td>
-                    <td>17:00</td>
-                </tr>
-
+                    <tr>
+                        <td>FORMULA 1 ETIHAD AIRWAYS ABU DHABI GRAND PRIX 2026</td>
+                        <td>Abu Dhabi</td>
+                        <td>04 - 06</td>
+                        <td>17:00</td>
+                    </tr>
+                </tbody>
                 <colgroup>
                     <col id="schedule-gp-column" />
                     <col id="schedule-location-column" />
