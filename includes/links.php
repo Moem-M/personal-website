bc13d5660b21b42c6b72f3874e5ff4ee467f9a86
Moem <156375920+Moem-M@users.noreply.github.com>
2026-04-30 22:54:01 +0300

    added last links

--- includes/links.php
+++ includes/links.php
@@ -2,8 +2,8 @@
     <ul>
         <li><a href="../pages/gallery.php">Picture Gallery</a></li>
         <li><a href="../pages/schedule.php">F1 Schedule</a></li>
-        <li><a>Video</a></li>
-        <li><a>Database</a></li>
+        <li><a href="../pages/video.php">Video</a></li>
+        <li><a href="../pages/database.php">Database</a></li>
         <li><a href="../pages/feedback.php">Feedback</a></li>
         <li><a href="../pages/resume.php">Resume</a></li>
     </ul>
